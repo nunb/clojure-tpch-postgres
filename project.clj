071de5c33dced4d7bc3ed18e29c3142c453a995e
Sanchayan Maity <maitysanchayan@gmail.com>
2017-03-20 11:32:35 +0530

    Update Clojure

--- project.clj
+++ project.clj
@@ -3,7 +3,7 @@
   :url "https://github.com/SanchayanMaity/clojure-tpch-postgres"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.9.0-alpha14"]
+  :dependencies [[org.clojure/clojure "1.9.0-alpha15"]
                  [org.clojure/java.jdbc "0.7.0-alpha1"]
                  [postgresql "9.3-1102.jdbc41"]]
   :main jdbc.core
